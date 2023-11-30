01823cb1c70388ef56f26980b1599c17cef41a21
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-11-30 11:43:32 +0330

    add profile model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User,Profile
 
 
 class CustomUserAdmin(UserAdmin):
@@ -63,3 +63,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)

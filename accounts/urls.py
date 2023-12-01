3bca7b5aabc52eca6a782d3b41f7e87aa5fc3827
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-01 10:41:58 +0330

    add index_page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-
+from .views import index_page
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path('index/',index_page,name='index'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index_page(request):
+    return HttpResponse('<h1>This is index page</h1>')

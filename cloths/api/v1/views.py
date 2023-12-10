de41d82cc48e0b6cd1703b2eaa52bf251e6d356f
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-10 20:11:00 +0330

    add permission classes and pagination class

--- cloths/api/v1/views.py
+++ cloths/api/v1/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from cloths.models import Cloth
 from .serializers import ClothSerializer
+from .paginations import DefaultPagination
 
 
 class ClothModelViewSet(viewsets.ModelViewSet):
     serializer_class = ClothSerializer
     queryset = Cloth.objects.filter(active=True)
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = DefaultPagination

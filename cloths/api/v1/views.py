69fb703aa8b8d7581545afc62c70747f0d197943
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-10 20:38:43 +0330

    add new permission

--- cloths/api/v1/views.py
+++ cloths/api/v1/views.py
@@ -3,10 +3,11 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from cloths.models import Cloth
 from .serializers import ClothSerializer
 from .paginations import DefaultPagination
+from .permissions import IsOwnerOrReadOnly
 
 
 class ClothModelViewSet(viewsets.ModelViewSet):
     serializer_class = ClothSerializer
     queryset = Cloth.objects.filter(active=True)
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     pagination_class = DefaultPagination

67f1e5288d8e7c1ef9e736118d63be92b560effa
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-14 13:25:34 +0330

    add filter backends

--- cloths/api/v1/views.py
+++ cloths/api/v1/views.py
@@ -4,6 +4,8 @@ from cloths.models import Cloth
 from .serializers import ClothSerializer
 from .paginations import DefaultPagination
 from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class ClothModelViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,9 @@ class ClothModelViewSet(viewsets.ModelViewSet):
     queryset = Cloth.objects.filter(active=True)
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend,SearchFilter, OrderingFilter]
+    filterset_fields = {
+        "author": ["exact", "in"],
+    }
+    search_fields = ["title","description"]
+    ordering_fields = ["datetime_created"]
--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     #DRF
     'rest_framework',
-
     # my apps
     'accounts',
     'cloths',

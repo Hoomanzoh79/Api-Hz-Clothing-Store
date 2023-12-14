726da217a5e38b0c56d468e9324b1cb1f8fe41c1
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-14 14:22:41 +0330

    add author to read only fields

--- cloths/api/v1/serializers.py
+++ cloths/api/v1/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from cloths.models import Cloth
-
+from accounts.models import Profile
 
 class ClothSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,10 @@ class ClothSerializer(serializers.ModelSerializer):
                   'gender',
                   'datetime_created',
                   'datetime_modified',]
+        
+        read_only_fields = ['author']
+    
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["author"] = Profile.objects.get(user_id=request.user.id)
+        return super().create(validated_data)

c01ebde4bbf7362bfb58f9d054ee5c6ae8eea2b2
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-14 14:59:53 +0330

    add absolute url to serializer fields

--- cloths/api/v1/serializers.py
+++ cloths/api/v1/serializers.py
@@ -3,6 +3,8 @@ from cloths.models import Cloth
 from accounts.models import Profile
 
 class ClothSerializer(serializers.ModelSerializer):
+    # relative_path = serializers.URLField(source='api_get_absolute_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_absolute_url',)
     class Meta:
         model = Cloth
         fields = ['id',
@@ -13,6 +15,7 @@ class ClothSerializer(serializers.ModelSerializer):
                   'active',
                   'season',
                   'gender',
+                  'absolute_url',
                   'datetime_created',
                   'datetime_modified',]
         
@@ -22,3 +25,7 @@ class ClothSerializer(serializers.ModelSerializer):
         request = self.context.get("request")
         validated_data["author"] = Profile.objects.get(user_id=request.user.id)
         return super().create(validated_data)
+    
+    def get_absolute_url(self,obj):
+        request = self.context.get("request")
+        return request.build_absolute_uri(obj.pk)

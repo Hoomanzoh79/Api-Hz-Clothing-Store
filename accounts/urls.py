51344198b0de9381b7b5ffd1bcfb76c4a006d78d
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-16 09:49:27 +0330

    include new path for api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path,include
 from .views import index_page
 
+app_name = 'accounts'
+
 urlpatterns = [
+    # basic auth
     path("", include("django.contrib.auth.urls")),
+    # index
     path('index/',index_page,name='index'),
+    path('api/v1/',include('accounts.api.v1.urls')),
 ]

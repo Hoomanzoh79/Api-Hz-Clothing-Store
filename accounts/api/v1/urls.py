3132210346b7876b64d25d92481e539fc0ef4649
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-22 22:47:32 +0330

    add password change path

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     path("jwt/create/",views.CustomTokenObtainPairView.as_view(),name="jwt-create",),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
+
+    # change password
+    path("password/change/",views.ChangePasswordApiView.as_view(),name='change-password'),
 ]

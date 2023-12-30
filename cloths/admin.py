6f955f13d7b9052c4df8857ac1eb05c1b48e7da9
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-30 12:36:21 +0330

    add Comment model

--- cloths/admin.py
+++ cloths/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Cloth
+from .models import Cloth,Comment
 
 @admin.register(Cloth)
 class ClothAdmin(admin.ModelAdmin):
     list_display = ['title','author','price', 'active', 'season', 'gender', ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["cloth","author","body","active",]

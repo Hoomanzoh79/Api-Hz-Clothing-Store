d5711fd51565dbeac2bf5397b7904005330027e5
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-01-06 12:59:02 +0330

    add new paths for comments

--- cloths/api/v1/urls.py
+++ cloths/api/v1/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 app_name = 'api-v1'
 
@@ -7,3 +8,8 @@ router = DefaultRouter()
 router.register("cloth", views.ClothModelViewSet, basename="cloth")
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("comment_list/cloth/<int:cloth_id>/", views.CommentListCreateView.as_view(), name="comment_list"),
+    path("comment_detail/cloth/<int:cloth_id>/comment/<int:comment_id>/", views.CommentDetailView.as_view(), name="comment_detail"),
+]

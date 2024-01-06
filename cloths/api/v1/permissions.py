7d7a8b98ae15c23d9a9d36b54ef767f1c1e4f062
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-01-06 12:58:17 +0330

    add new permission

--- cloths/api/v1/permissions.py
+++ cloths/api/v1/permissions.py
@@ -1,10 +1,11 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnlyForProfile(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
+    ** for models that author is based on Profile 
     """
 
     def has_object_permission(self, request, view, obj):
@@ -15,3 +16,19 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Instance must have an attribute named `author`.
         return obj.author.user == request.user
+
+class IsOwnerOrReadOnlyForUser(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    ** for models that author is based on User
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `author`.
+        return obj.author == request.user

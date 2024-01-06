da6dfb5e60a90f96a56179c1d411d1e6c686e4cf
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-01-06 12:59:54 +0330

    change author to User

--- cloths/models/comments.py
+++ cloths/models/comments.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 from .cloths import Cloth
+from accounts.models import User
 
 class ActiveCommentsManager(models.Manager):
     def get_queryset(self):
         return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
 
 class Comment(models.Model):
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comments')
     cloth = models.ForeignKey(Cloth, on_delete=models.CASCADE, related_name='comments')
     body = models.TextField(verbose_name=_('please write your opinion here'))
     active = models.BooleanField(default=True)

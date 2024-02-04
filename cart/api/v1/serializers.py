8c83ec1f64c4f9597e7b2fe788d059a18878bb4f
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-04 20:06:04 +0330

    add CartItemSerializer

--- cart/api/v1/serializers.py
+++ cart/api/v1/serializers.py
@@ -2,8 +2,16 @@ from rest_framework.serializers import ModelSerializer
 from cart.models import Cart,CartItem
 
 
+
+class CartItemSerializer(ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ["cart","cloth","quantity"]
+
+
 class CartSerializer(ModelSerializer):
+    items = CartItemSerializer(many=True)
     class Meta:
         model = Cart
-        fields = ["id",]
-        read_only_fields = ["id"]
+        fields = ["id","items"]
+        read_only_fields = ["id","items"]

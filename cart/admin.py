adc62375276481592193b0319ee4dcb925a59cc1
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-04 18:09:52 +0330

    add CartItem

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from .models import Cart
+from .models import Cart,CartItem
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    fields = ['id', 'cloth', 'quantity']
+    extra = 0
+    min_num = 1
+
 
 @admin.register(Cart)
-class CartAdmin(ModelAdmin):
-    list_display = ["created_at"]
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created_at']
+    inlines = [CartItemInline]
--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from uuid import uuid4
+from cloths.models import Cloth
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(primary_key=True,default=uuid4)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    cloth = models.ForeignKey(Cloth, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'cloth']]

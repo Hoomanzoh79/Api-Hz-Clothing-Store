be78996afa1b47f8518893ec72a14708ed019bdc
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-06 18:12:29 +0330

    add template render to test debug-toolbar

--- cloths/urls.py
+++ cloths/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from .views import HelloPageView
 
 app_name = "cloths"
 
 urlpatterns = [
     path("api/v1/", include("cloths.api.v1.urls")),
+    path("hello/",HelloPageView.as_view(),name="hello"),
 ]
--- cloths/views.py
+++ cloths/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HelloPageView(TemplateView):
+    template_name = "cloths/hello.html"

fcc719bc897c32d2344cd9b4f683ce8d7715aede
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-10 13:47:51 +0330

    add 2 serializers to CartItemViewSet

--- cart/api/v1/views.py
+++ cart/api/v1/views.py
@@ -2,16 +2,23 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import CartSerializer,CartItemSerializer
+from .serializers import CartSerializer,CartItemSerializer,AddCartItemSerializer
 from cart.models import Cart,CartItem
 
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
     
     def get_queryset(self):
         cart_pk = self.kwargs["cart_pk"]
         return CartItem.objects.select_related("cloth").filter(cart_id=cart_pk).all()
+    
+    def get_serializer_class(self):
+        if self.request.method=="POST":
+            return AddCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {"cart_pk":self.kwargs["cart_pk"]}
 
 class CartViewSet(mixins.CreateModelMixin,
                   mixins.DestroyModelMixin,

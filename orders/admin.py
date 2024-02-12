6c4f8dbee255c3bae16fcfdfbdb84dd3c1e45292
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-12 12:40:13 +0330

    add Order and OrderItem model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    list_display = ['order', 'cloth', 'quantity', 'price', ]
+    extra = 0
+    min_num = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'first_name', 'last_name', 'datetime_created', 'is_paid','is_cancelled']
+    inlines = [
+        OrderItemInline
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'cloth', 'quantity', 'price', ]
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Order(models.Model):
+    customer = models.ForeignKey(get_user_model(), on_delete=models.PROTECT,related_name="orders")
+    is_paid = models.BooleanField(default=False)
+    is_cancelled = models.BooleanField(default=False)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=700)
+    order_notes = models.CharField(max_length=700, blank=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_price(self):
+        return sum(item.price * item.quantity for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    cloth = models.ForeignKey('cloths.Cloth', on_delete=models.PROTECT, related_name='order_items')
+    quantity = models.PositiveSmallIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = [['order', 'cloth']]
+
+    def __str__(self):
+        return f'OrderItem {self.id}: {self.cloth} x {self.quantity} (price:{self.price})'
+

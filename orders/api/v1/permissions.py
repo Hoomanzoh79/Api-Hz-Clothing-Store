6cee66df7a81374cc64d034c6487a6e87cebf779
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-14 14:27:23 +0330

    add new permission

---
+++ orders/api/v1/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    ** for models that author is based on User
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `author`.
+        return obj.customer.is_superuser & obj.customer.is_staff == True 

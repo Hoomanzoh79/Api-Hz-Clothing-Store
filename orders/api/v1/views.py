73a99305978559385314d31bb48a131a07d0fd09
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-14 14:27:09 +0330

    change OrderViewSet

--- orders/api/v1/views.py
+++ orders/api/v1/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated,DjangoModelPermissions
+from .permissions import IsAdminOrReadOnly
 
 
 from .serializers import OrderSerializer
@@ -7,5 +9,12 @@ from orders.models import Order,OrderItem
 
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
-    queryset = Order.objects.filter(is_cancelled=False)
+    permission_classes = [IsAuthenticated,IsAdminOrReadOnly]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser & user.is_staff:
+            return Order.objects.filter(is_cancelled=False)
+        else:
+            return Order.objects.filter(is_cancelled=False,customer=user)
         

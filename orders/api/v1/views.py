627710e7a9a889948c56ed720a1c3992bdf90dc6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-16 20:47:45 +0330

    add OrderItemViewSet and fix some similar queries in OrderViewSet

--- orders/api/v1/views.py
+++ orders/api/v1/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated,DjangoModelPermissions
 from .permissions import IsAdminOrReadOnly
 
 
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer,OrderItemSerializer
 from orders.models import Order,OrderItem
 
 
@@ -14,7 +14,12 @@ class OrderViewSet(ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         if user.is_superuser & user.is_staff:
-            return Order.objects.filter(is_cancelled=False)
+            return Order.objects.prefetch_related("items__cloth").select_related("customer").filter(is_cancelled=False)
         else:
-            return Order.objects.filter(is_cancelled=False,customer=user)
+            return Order.objects.prefetch_related("items__cloth").select_related("customer").filter(is_cancelled=False,customer=user)
+
+
+class OrderItemViewSet(ModelViewSet):
+    serializer_class = OrderItemSerializer
+    queryset = OrderItem.objects.all()
         

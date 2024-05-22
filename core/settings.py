32f3ba7daf29baed231a4e9ec35c2f6f809dddc3
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-22 17:28:04 +0330

    add env vars for database configs

--- core/settings.py
+++ core/settings.py
@@ -98,12 +98,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': 'db',
-        'PORT': 5432
+        'PORT': 5432,
     }
 }
 

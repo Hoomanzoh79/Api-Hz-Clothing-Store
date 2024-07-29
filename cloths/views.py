7a1b63750788b07b5d2c6e8dbbe8ca0e67aa44fc
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-07-29 15:48:32 +0330

    comment show_data

--- cloths/views.py
+++ cloths/views.py
@@ -1,6 +1,17 @@
+from django.db import transaction,connection
+from django.db.models import Count,ExpressionWrapper,F,DecimalField
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
+from accounts.models import Profile
+from cloths.models import Cloth
+from orders.models import Order,OrderItem
 
-class HelloPageView(TemplateView):
-    template_name = "cloths/hello.html"
+# @transaction.atomic
+# def show_data(request):
+#     # queryset = OrderItem.objects.annotate(total_price=ExpressionWrapper(F('price')*0.95,output_field=DecimalField()))
+#     # queryset = list(Cloth.objects.raw("SELECT title,id FROM cloths_cloth"))
+#     with connection.cursor() as cursor:
+#         cursor.callproc(procname="testfunc")
+#         results = cursor.fetchall()
+
+#     return render(request,"cloths/hello.html")
